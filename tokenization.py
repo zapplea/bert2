b6c1c6aee11dea87590f8cc5a094cf6fe0c0bfe3
yibing <zapplea@gmail.com>
2019-03-10 14:17:10 +0800

    add

--- tokenization.py
+++ tokenization.py
@@ -177,8 +177,8 @@ class FullTokenizer(object):
       for sub_token in self.wordpiece_tokenizer.tokenize(token):
         print(sub_token)
         split_tokens.append(sub_token)
-      print(split_tokens)
-      exit()
+    print(split_tokens)
+    exit()
 
     return split_tokens
 
