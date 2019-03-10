a8299c07587fa21c13bbdce95dc67a7d6a0a77cc
yibing <zapplea@gmail.com>
2019-03-10 13:46:35 +0800

    add

---
+++ generate_train_dataset.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 create_pretraining_aic2018data.py --input_file '/datastore/liu121/sentidata2/data/sentences.txt,/datastore/liu121/sentidata2/data/sentences2.txt' --output_file '/datastore/liu121/sentidata2/data/sent_output.txt' --vocab_file '/datastore/liu121/sentidata2/data/sent_vocab.txt'
